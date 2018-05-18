fa5ec2583df5b2cc5dbd05de0839b61e041b7601
T-101 <T-101@PC227.mshome.net>
2018-05-17 19:47:26 -0500

    solucion conversion parte 1

---
+++ Objetos-1803/src/abstraccion/Grados.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package abstraccion;
+
+/**
+ *
+ * @author T-101
+ */
+public class Grados {
+ private float centigrados;
+ private float farenheit;
+
+ public void setcentigrados (float centigrados){
+     this.centigrados = centigrados;
+ }
+ public float getCentigrados(){
+     return centigrados;
+ }
+ public void setfarenheit (float farenheit){
+     this.farenheit=farenheit;
+}
+}
